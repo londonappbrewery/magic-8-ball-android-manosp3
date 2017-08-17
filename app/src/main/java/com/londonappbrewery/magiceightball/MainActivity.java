58766e9abd53bda496b254c360d28e7ce8c3cfe6
manosp3 <emm.perakis@gmail.com>
2017-08-17 13:38:42 +0300

    Udemy Android App Development course challenge #2

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,7 +1,14 @@
 package com.londonappbrewery.magiceightball;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button  askBtn;
+        askBtn = (Button) findViewById(R.id.askBtn);
+
+        final ImageView ballDisplay;
+        ballDisplay = (ImageView) findViewById(R.id.ballImg);
+
+        /* ftiaksame ena array*/
+        final int[] ballArray = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+        /*ftiaksame ena koumpi, prepei na tou valoume to listener gia na akouei otan to patame*/
+        askBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                /*Etsi ftiaxnoume random numbers*/
+                Random randomNumber = new Random();
+                int number = randomNumber.nextInt(5);
+
+                ballDisplay.setImageResource(ballArray[number]);
+
+            }
+        });
+
+
     }
 }
